b85bd81d48b12f66f8e205826b7cce3a4c328bff
Soher29 <soher29>
2020-02-26 19:28:53 +0200

    v2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const users = require('./users');
 const books = require('./book')
+const boolshelf = require('./bookshelf');
 
 module.exports = function(app) {
   users(app)
   books(app)
+  bookshelf(app)
 
   return app;
 };
